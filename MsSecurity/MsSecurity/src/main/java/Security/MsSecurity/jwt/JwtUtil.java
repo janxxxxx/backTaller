1067d165df7bfc5a31e2713dafecb3600419839e
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2025-01-07 12:30:59 -0500

    Create JwtUtil.java

---
+++ MsSecurity/MsSecurity/src/main/java/Security/MsSecurity/jwt/JwtUtil.java
@@ -0,0 +1,32 @@
+package Security.MsSecurity.jwt;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    private final String SECRET_KEY = "mySecretKey";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
