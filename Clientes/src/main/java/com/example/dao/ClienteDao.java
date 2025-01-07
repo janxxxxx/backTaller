76d9b5bd9cac8ef08270af415c7c5f6bce417c48
rparedesm99 <romerparedesm@gmail.com>
2025-01-07 18:25:27 -0500

    client dao

--- Clientes/src/main/java/com/example/dao/ClienteDao.java
+++ Clientes/src/main/java/com/example/dao/ClienteDao.java
@@ -1,5 +1,16 @@
 package main.java.com.example.dao;
 
-public class ClienteDao {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ClienteDao extends JpaRepository<Cliente, Long>{
+
+    //El query no puede trabajar con la lista Page, entonces aqui creo el list
     
+    @Query(value = "SELECT * FROM clientes WHERE clientes.nombre union SELECT * FROM clientes WHERE clientes.tipofactura LIKE %?1%", nativeQuery = true)
+    Page<Cliente> findAll(Pageable pageable, String palabraClave);
 }
