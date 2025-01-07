2b0c72aab04e4ed70f2563dc9d64d6009351af2d
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2025-01-07 12:30:42 -0500

    Create UserService.java

---
+++ MsSecurity/MsSecurity/src/main/java/Security/MsSecurity/service/UserService.java
@@ -0,0 +1,27 @@
+package Security.MsSecurity.service;
+
+import java.util.Optional;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private Security.MsSecurity.repository.UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+}
