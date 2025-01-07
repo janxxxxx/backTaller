ae0cc377f58384581ef0f73810798121ef4838ec
janxxxxx <>
2025-01-07 18:29:25 -0500

    provedores

---
+++ MsProvedores/src/test/java/PROVEDOR/PROVEDOR/ProvedorApplicationTests.java
@@ -0,0 +1,13 @@
+package PROVEDOR.PROVEDOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
