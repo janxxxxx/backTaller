e12f6a6d6ddaef5d3e1aed3e320beeffd1dc597a
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2025-01-07 17:27:08 -0500

    Update AuthRequest.java

--- Security/src/main/java/POLYGLOT/MS/TEST/Security/dto/AuthRequest.java
+++ Security/src/main/java/POLYGLOT/MS/TEST/Security/dto/AuthRequest.java
@@ -7,7 +7,6 @@ public class AuthRequest implements Serializable {
     private static final long serialVersionUID = 1L;
     private String username;
     private String password;
-    private String rol;
  
     public AuthRequest() {
  
