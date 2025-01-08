2c8e459361f23cdc7694f90ee1e82fe1a51e7f7c
janxxxxx <>
2025-01-07 19:08:09 -0500

    repository

---
+++ MsProvedores/src/main/repository/IInvoiceRepository.java
@@ -0,0 +1,12 @@
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import POLYGLOT.MS.TEST.Invoice.model.InvoiceModel;
+
+
+@Repository
+public interface IInvoiceRepository extends CrudRepository<InvoiceModel,Integer> {
+   
+}
+
+
+
