a26208a47cf5298d2f8d117d5beca102db6dfa84
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2025-01-11 11:51:21 -0500

    Valores constantes para las rutas

---
+++ Access/Access/src/main/java/MsSecurity/Access/constanst/NoHardCodeo.java
@@ -0,0 +1,10 @@
+package MsSecurity.Access.constanst;
+
+public class NoHardCodeo {
+    public static final String ALUMNO_BASE = "alumno";
+    public static final String GET_ALL = "/findAll";
+    public static final String CREATE = "/create";
+    public static final String GET_BY_ID = "/findById/{id}";
+    public static final String UPDATE = "/update";
+    public static final String DELETE = "/delete/{id}";
+}
