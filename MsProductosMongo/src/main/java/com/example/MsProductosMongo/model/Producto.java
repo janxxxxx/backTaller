00dc8754b285222b07e1c046cf222d56e5f4b219
Jose Mori <antoniomori2093@gmail.com>
2025-01-21 02:35:57 -0500

    Subida_Productos_MongoDB

---
+++ MsProductosMongo/src/main/java/com/example/MsProductosMongo/model/Producto.java
@@ -0,0 +1,59 @@
+package com.example.MsProductosMongo.model;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "productos")
+public class Producto {
+
+    @Id
+    private ObjectId id; // Cambiar a ObjectId
+
+    private String codigo;
+    private String descripcion;
+    private String categoria;
+    private String almacen;
+
+    // Constructor, getters y setters
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getAlmacen() {
+        return almacen;
+    }
+
+    public void setAlmacen(String almacen) {
+        this.almacen = almacen;
+    }
+}
---
+++ MsProductosMongo/src/test/java/com/example/MsProductosMongo/MsProductosMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MsProductosMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductosMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
