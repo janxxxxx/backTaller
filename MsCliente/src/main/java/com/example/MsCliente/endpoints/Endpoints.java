2d420c8a23ff71aa2f73e82ee2cc4ff05df9a42c
franklinsunus <franklincuentaunu@gmail.com>
2025-01-21 12:38:17 -0500

    MsClientev2

---
+++ MsCliente/src/main/java/com/example/MsCliente/endpoints/Endpoints.java
@@ -0,0 +1,10 @@
+package com.example.MsCliente.endpoints;
+
+public class Endpoints {
+    public static final String CLIENTES = "/clientes";
+    public static final String CLIENTES_FIND_ALL = "/findAll";
+    public static final String CLIENTES_CREATE = "/create";
+    public static final String CLIENTES_BY_ID = "/{id}";
+    public static final String CLIENTES_UPDATE = "/{id}";
+    public static final String CLIENTES_DELETE = "/{id}";
+}
