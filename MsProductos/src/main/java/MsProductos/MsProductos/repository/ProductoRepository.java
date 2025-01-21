079e5712a81ceff22b4fcc5c910da1c8ea3f8841
Jose Mori <antoniomori2093@gmail.com>
2025-01-21 02:33:48 -0500

    Eliminacion_Productos

--- MsProductos/src/main/java/MsProductos/MsProductos/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package MsProductos.MsProductos.repository;
-
-import MsProductos.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
